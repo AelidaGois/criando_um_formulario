8446f773eb060fb8074afe36d8944ea6db26c3e6
Aelida <lidasouza89@gmail.com>
2022-05-06 13:34:53 -0300

    indentação

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-
-// let btnEnter = document.getElementById ('btnEnter');
-
-
-function VerifyUser() {
+function VerifyUser() {// let btnEnter = document.getElementById ('btnEnter');
   console.log('clicked')
   if (document.getElementById ('email').value === 'tryber@teste.com' && document.getElementById ('password').value === '123456') {
     alert('Olá, Tryber!');
   }
   else {
-    alert('Email ou senha inválidos.')
+    alert('Email ou senha inválidos.');
   }
-}
+}VerifyUser();
 
 function enableBtn() {
-  document.getElementById('submit-btn').disabled = false
+  document.getElementById('submit-btn').disabled = false;
 }
 
 // document.getElementById ('btnEnter').addEventListener('click', VerifyUser)
